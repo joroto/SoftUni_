54311ce6c8d2e178fe560104934659062aa80ccb
joroto <jojo_to@abv.bg>
2021-02-20 16:06:31 -0800

    20/02/2021 Advanced

---
+++ Java_Advanced/13_Generics_Lab/Generic_Array_Creator/ArrayCreator.java
@@ -0,0 +1,22 @@
+package Generic_Array_Creator;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayCreator <T>{
+
+    @SuppressWarnings("uncheked")
+     public static <T> T[] create(int length, T item) {
+         T[] arr = (T[]) Array.newInstance(item.getClass(), length);
+         Arrays.fill(arr, item);
+
+         return arr;
+    }
+
+    public static <T> T[] create(Class<T> clazz, int length, T item) {
+        T[] arr = (T[]) Array.newInstance(clazz, length);
+        Arrays.fill(arr, item);
+
+        return arr;
+    }
+}
---
+++ Java_Advanced/13_Generics_Lab/Generic_Array_Creator/Main.java
@@ -0,0 +1,9 @@
+package Generic_Array_Creator;
+
+public class Main {
+    public static void main(String[] args) {
+         Integer[] array = ArrayCreator.create(13,13);
+         String[] array2 = ArrayCreator.create(3, "Hello");
+
+    }
+}
