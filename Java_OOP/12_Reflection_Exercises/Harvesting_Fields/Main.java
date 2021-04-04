bb7f33e74ec73e3cc88417ec1af5164953b66b27
joroto <jojo_to@abv.bg>
2021-04-05 00:39:22 +0300

    04/04/2021 OOP

---
+++ Java_OOP/12_Reflection_Exercises/Harvesting_Fields/Main.java
@@ -0,0 +1,29 @@
+package Harvesting_Fields;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		String input;
+		while (!(input = scan.nextLine()).equals("HARVEST")){
+
+			Field[] fields = RichSoilLand.class.getDeclaredFields();
+			String finalInput1 = input;
+
+			if(input.equals("all")){
+				Arrays.stream(fields).forEach(Main::print);
+			}
+			Arrays.stream(fields).filter(f -> (Modifier.toString(f.getModifiers())).equals(finalInput1))
+						.forEach(Main::print);
+		}
+	}
+	private static String print(Field field){
+		System.out.println(Modifier.toString(field.getModifiers()) + " "+field.getType().getSimpleName() + " " + field.getName());
+		return field.toString();
+	}
+}
