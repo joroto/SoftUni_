f1cc2d855eae68c27e75a59f8dcd51cf203dca46
joroto <jojo_to@abv.bg>
2021-04-07 23:12:33 +0300

    07/04/2021 OOP

---
+++ Java_OOP/12_Reflection_Exercises/barracksWars/core/factories/UnitFactoryImpl.java
@@ -0,0 +1,31 @@
+package barracksWars.core.factories;
+
+import barracksWars.interfaces.Unit;
+import barracksWars.interfaces.UnitFactory;
+import barracksWars.models.units.AbstractUnit;
+import jdk.jshell.spi.ExecutionControl;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class UnitFactoryImpl implements UnitFactory {
+
+	private static final String UNITS_PACKAGE_NAME =
+			"barracksWars.models.units.";
+
+	@Override
+	public Unit createUnit(String unitType) throws ExecutionControl.NotImplementedException {
+		Unit unit;
+
+		try {
+			Class<?> clazz = Class.forName(UNITS_PACKAGE_NAME + unitType);
+			unit = (Unit) clazz.getDeclaredConstructor().newInstance();
+		} catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+			//?
+			throw new IllegalStateException(e);
+		}
+
+		return unit;
+
+
+	}
+}
